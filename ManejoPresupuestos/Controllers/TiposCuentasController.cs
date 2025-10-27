0daf03897ee7214a15fe42d8da9eb4f441bc628d
BrayanRojasLemus <brayanrojas@wposs.com>
2025-10-27 11:49:10 -0500

    Agregar archivos de proyecto.

---
+++ ManejoPresupuestos/Controllers/TiposCuentasController.cs
@@ -0,0 +1,26 @@
+﻿using ManejoPresupuestos.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManejoPresupuestos.Controllers
+{
+    public class TiposCuentasController : Controller
+    {
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Crear(TipoCuenta tipoCuenta)
+        {
+            if (!ModelState.IsValid) 
+            { 
+                return View(tipoCuenta);
+            }
+            return View();
+        }
+
+
+    }
+}
