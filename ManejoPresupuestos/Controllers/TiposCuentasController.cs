7724f06d75e1ffe9d60972caa197b6ef918f20a9
BrayanRojasLemus <brayanrojas@wposs.com>
2025-12-12 08:17:39 -0500

    Añadir soporte para Dapper y conexión a la base de datos
    Se ha implementado el uso de Dapper y Microsoft.Data.SqlClient en el controlador `TiposCuentasController.cs` para realizar consultas a la base de datos. Se ha agregado una cadena de conexión en `appsettings.Development.json` y se ha modificado el método `Crear` para establecer la conexión y ejecutar una consulta simple. Además, se ha comentado la validación personalizada en el modelo `TipoCuenta` y se han añadido las referencias necesarias a los paquetes en el archivo del proyecto `ManejoPresupuestos.csproj`.

--- ManejoPresupuestos/Controllers/TiposCuentasController.cs
+++ ManejoPresupuestos/Controllers/TiposCuentasController.cs
@@ -1,13 +1,28 @@
-﻿using ManejoPresupuestos.Models;
+﻿using Dapper;
+using ManejoPresupuestos.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 
 namespace ManejoPresupuestos.Controllers
 {
     public class TiposCuentasController : Controller
     {
+
+        private readonly string connectionString;
+
+        public TiposCuentasController(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
         public IActionResult Crear()
         {
-            return View();
+            using (var connection = new SqlConnection(connectionString))
+            { 
+                var query = connection.Query("SELECT 1").FirstOrDefault();
+            }
+
+                return View();
         }
 
 
--- ManejoPresupuestos/Models/TipoCuenta.cs
+++ ManejoPresupuestos/Models/TipoCuenta.cs
@@ -4,29 +4,29 @@ using System.Reflection.Metadata.Ecma335;
 
 namespace ManejoPresupuestos.Models
 {
-    public class TipoCuenta : IValidatableObject
+    public class TipoCuenta //: IValidatableObject
     {
         public int Id { get; set; }
 
         [Required(ErrorMessage = "El campo {0} es requerido")]
-        //[/*PrimeraLetraMayuscula*/]
+        [PrimeraLetraMayuscula]
         //[StringLength(maximumLength: 50, MinimumLength = 3, ErrorMessage = "La longitud del campo es {0} debe estar entre {2} y {1}")]
         //[Display(Name = "Nombre del tipo cuenta")]
         public string Nombre { get; set; }
         public int UsuarioId { get; set; }
         public int Orden { get; set; }       
       
-        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        {
-            if (Nombre != null && Nombre.Length > 0)
-            { 
-                var primeraLetra = Nombre[0].ToString();
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+        //    if (Nombre != null && Nombre.Length > 0)
+        //    { 
+        //        var primeraLetra = Nombre[0].ToString();
 
-                if (primeraLetra != primeraLetra.ToUpper())
-                { 
-                    yield return new ValidationResult("La primera letra debe ser mayúscula", new[] { nameof(Nombre) } );
-                }
-            }
-        }
+        //        if (primeraLetra != primeraLetra.ToUpper())
+        //        { 
+        //            yield return new ValidationResult("La primera letra debe ser mayúscula", new[] { nameof(Nombre) } );
+        //        }
+        //    }
+        //}
     }
 }
